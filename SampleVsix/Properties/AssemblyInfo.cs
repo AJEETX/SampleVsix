80ec85491f4487577c3d6bb2a7a5780b949ce102
Ajeet <ajeet.tiwari@helloworld.com.au>
2018-03-16 21:34:45 +1100

    renamed folder

--- SampleVsix/Properties/AssemblyInfo.cs
+++ SampleVsix/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmptyVsix")]
-[assembly: AssemblyDescription("Empty Vsix ")]
+[assembly: AssemblyTitle("SampleVsix")]
+[assembly: AssemblyDescription("Create an Visual Studio extension through Vsix template")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EmptyVsix")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("its aby  pty ltd")]
+[assembly: AssemblyProduct("SampleVsix")]
+[assembly: AssemblyCopyright("its aby @ copyright 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,3 +32,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-AU")]
+
